3903c11a9a5e4fc63911a411fe6daa59421b6c40
JianchaoLai <laijianchao37@gmail.com>
2015-06-19 23:50:51 -0500

    coursera R programming class assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## making a vector of inverse matrix of all the input matrix and read the cached matrix if it has been processed before
 
-## Write a short comment describing this function
+##getting the inverse of matrix and caching that inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	I<-NULL
+	set <- function(y) {
+		x <<- y
+		I <<-NULL
+	}
+	get <- function()x
+	setinv <- function(solve) I<<- solve
+	getinv <- function() I
+	list( set = set, get = get,
+		  setinv = setinv,
+		  getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## testing whether there is a cached inversed matrix and get the cached matrix if there is one, otherwise calculates the data
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        I <- x$getinv()
+        if (!is.null(I)) {
+        	message("getting cached data")
+        	return(I)
+        }
+        data <- x$get()
+        I <- solve(data,...)
+        x$setinv(I)
+        I
 }
